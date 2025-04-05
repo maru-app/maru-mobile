645e62e13f9849ad92c44ca865f92a6770b35f0e
Daegyeom Ha <combbm@gmail.com>
2025-04-05 21:25:08 +0900

    feat: add global font with tailwindcss

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,40 @@
-import { Stack } from "expo-router";
+import './global.css';
+
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    Pretendard: require('@/assets/fonts/PretendardVariable.ttf'),
+    KCCDodamdodam: require('@/assets/fonts/KCCDodamdodam.otf')
+  });
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepareApp() {
+      if (fontsLoaded) setAppIsReady(true);
+    }
+
+    prepareApp();
+  }, [fontsLoaded]);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View onLayout={onLayoutRootView} className="flex-1">
+      <Stack />
+    </View>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  presets: [require('nativewind/preset')],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        pretendard: ['Pretendard', 'sans-serif'],
+        dodam: ['KCCDodamdodam', 'sans-serif']
+      }
+    }
   },
   plugins: []
 };
